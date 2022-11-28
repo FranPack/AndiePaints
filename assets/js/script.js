62eb9e9bb52c5dc77de0c76dfd3176cd4fc2347d
FranPack <59519630+FranPack@users.noreply.github.com>
2022-11-27 23:57:18 -0500

    gallery touchup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-// var navheight = $("#navigation").height()
-// console.log(navheight)
-// $(window).scroll(function() {
-//     if ($(this).scrollTop() > navheight){  
-//         $('body').addClass("sticky");
-//     }
-//     else{
-//         $('body').removeClass("sticky");
-//     }
-// });
+var navheight = $("#navigation").height()
+console.log(navheight)
+$(window).scroll(function() {
+    if ($(this).scrollTop() > navheight){  
+        $('body').addClass("sticky");
+    }
+    else{
+        $('body').removeClass("sticky");
+    }
+});
